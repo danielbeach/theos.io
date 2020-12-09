c65d065dfd8d35fc03f54cae7d7f9b088225a7c9
danielbeach <danielbeach@granular.ag>
2020-12-09 13:29:27 -0600

    possible datapoints for sql schema

---
+++ SQLish/schema.sql
@@ -0,0 +1,15 @@
+"""
+    Database data points that we will gather.
+    - book_id
+    - author
+    - author_id
+    - author_first_name
+    - author_last_name
+    - author_country
+    - title
+    - sub_title
+    - year_published
+    - church_period (reformation, puritan, early church)
+    - resource_type (book, letter, tract)
+    - ???
+"""
