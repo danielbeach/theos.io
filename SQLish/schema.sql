6a3ffc9c5c77df26930a0ffff0f34374dff308bb
danielbeach <danielbeach@granular.ag>
2020-12-11 13:35:11 -0600

    add some thoughts on possible schema.

--- SQLish/schema.sql
+++ SQLish/schema.sql
@@ -13,3 +13,32 @@
     - resource_type (book, letter, tract)
     - ???
 """
+
+CREATE SCHEMA theos;
+CREATE TABLE theos.authors
+    (
+        author_id SERIAL PRIMARY KEY,
+        first_name TEXT,
+        middle_name TEXT,
+        last_name TEXT,
+        full_name TEXT,
+        author_country_code INT,
+        born_year INT,
+        died_year INT
+    );
+CREATE TABLE theos.book_resource
+    (
+        resource_id SERIAL PRIMARY KEY,
+        resource_type TEXT, # book or letter etc.
+        title TEXT,
+        sub_title TEXT,
+        publish_year INT,
+        church_period INT
+    );
+CREATE TABLE theos.church_periods
+    (
+        church_period SERIAL PRIMARY KEY,
+        name TEXT,
+        year_start INT,
+        year_end INT,
+    );
